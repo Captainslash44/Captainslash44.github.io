08e9bb296e361d6f2ebc193f78979362c27e9805
Halim Njeim <michaelnjeim44@gmail.com>
2024-12-20 08:45:49 +0200

    declaring variables in JS

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-document.getElementById("prefix-input").disabled = true;
+let mode = false; // False is for prefix mode - True for postfix mode.
+let prefix_expression = [];
+let postfix_expression = [];
+
+
+
+let calculate_button;
+let toggle_button;
+let clear_button;
+let prefix_input;
+let postfix_input;
+
